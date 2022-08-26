9ea938b8e75f00e763467a49f9966395d84d9c10
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-08-26 15:07:34 +0200

    finish SVG painting

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-/**
- * Generates a random integer in a given interval, lower and upper bound included.
- *
- * Inpiration: https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
- *
- * @param {*} lower is the lower bound of the interval.
- * @param {*} upper is the upper bound of the interval.
- * @returns a random integer in the provided interval.
- */
-const randomIntegerInInterval = (lower, upper) =>
-  Math.floor(Math.random() * (upper - lower + 1) + lower);
-
 $(document).ready(() => {
   $("#docs-button").html("Vis dokumentasjon");
   $("#docs-container").hide();
@@ -37,6 +25,8 @@ $(document).ready(() => {
       "animation-direction": "reverse",
       "animation-duration": "4s",
     });
+
+    $("#svg-parent").addClass("move-around");
   });
 
   $("#svg-piece").mouseleave(() => {
@@ -45,5 +35,6 @@ $(document).ready(() => {
     $(".polygon.magenta").removeClass("flip");
     $(".circle.red").removeClass("scale");
     $(".square.orange").removeClass("skew");
+    $("#svg-parent").removeClass("move-around");
   });
 });
